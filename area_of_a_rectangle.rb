b3b4a3d7b7b9ec46ac8b532c1ea66b2aeeeb18a4
chuck1193 <>
2019-05-22 09:38:06 -0500

    Refactored the program

--- area_of_a_rectangle.rb
+++ area_of_a_rectangle.rb
@@ -1,17 +1,20 @@
-puts "What is the length of the room in feet?"
-length = gets.chomp.to_i
+def finding_the_area
+  puts "What is the length of the room in feet?"
+  length = gets.chomp.to_i
 
-puts "What is the width of the room in feet?"
-width = gets.chomp.to_i
+  puts "What is the width of the room in feet?"
+  width = gets.chomp.to_i
 
-area = length * width
-square_meters = area * 0.09290304
+  area = length * width
+  square_meters = area * 0.09290304
 
-if width.is_a? != Numeric || length.is_a? != Numeric
-  puts "Please enter a number"
-
+  if width =~ /\A[-+]?\d*\.?\d+\z/ && length =~ /\A[-+]?\d*\.?\d+\z/ 
+    puts "Please enter a number"
+    finding_the_area
+  elsif width.to_s !=~ /\A[-+]?\d*\.?\d+\z/ && length.to_s !=~ /\A[-+]?\d*\.?\d+\z/
+  puts "You entered in the dimensions of #{length} feet by #{width} feet."
+  puts "The area is #{area} square feet."
+  puts "#{square_meters} square meters"
+  end
 end
-
-puts "You entered in the dimensions of #{length} feet by #{width} feet."
-puts "The area is #{area} square feet."
-puts "#{square_meters} square metersr"
+finding_the_area
